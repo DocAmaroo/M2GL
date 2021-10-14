c32d5d3c7548abc622ab0493c5756603e8ba8aad
C.Thomas <thomas.canta@etu.umontpellier.fr>
2021-10-14 16:25:44 +0200

    :sparkles: Update

---
+++ HAI919I/tp/transformation-model/tp3/TransfoModel/src/transfo/Main.java
@@ -0,0 +1,111 @@
+package transfo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+
+import org.eclipse.emf.*;
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.*;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMLMapImpl;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.AggregationKind;
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.Class;
+import org.eclipse.uml2.uml.Comment;
+import org.eclipse.uml2.uml.Dependency;
+import org.eclipse.uml2.uml.DirectedRelationship;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Namespace;
+import org.eclipse.uml2.uml.Operation;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageableElement;
+import org.eclipse.uml2.uml.ParameterableElement;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Relationship;
+import org.eclipse.uml2.uml.StateMachine;
+import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.StringExpression;
+import org.eclipse.uml2.uml.TemplateParameter;
+import org.eclipse.uml2.uml.Type;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.Usage;
+import org.eclipse.uml2.uml.VisibilityKind;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Instruction récupérant le modèle sous forme d'arbre à partir de la classe racine "Model"
+		Model umlP = chargerModele("model/model.uml");
+		
+		
+		String nomModele=  umlP.getName();
+		
+		System.out.println("Ton modèle se nomme: \""+nomModele+"\"");
+		
+		
+		//Modifier le nom du modèle UML
+		umlP.setName("NewModelName");
+		//umlP.getMembers().forEach(System.out::println);
+		System.out.println("\""+nomModele+"\" Changé ! Le modèle se nomme: \""+umlP.getName()+"\"");
+		
+		getStateMachine(umlP, "PackageStateMachine", "State");
+		
+		//Sauvegarder le modèle avec son nouveau nom
+		sauverModele("model/changerNom.uml", umlP);
+		
+	}
+	
+	public static void getStateMachine(Model uml, String packageContainerName, String classname) {
+		Package currPackage = (Package) uml.getPackagedElement(packageContainerName);
+		PackageableElement classElement = currPackage.getPackagedElement(classname);
+		
+		System.out.println(classElement.getOwnedElements());
+	}
+		
+	public static void sauverModele(String uri, EObject root) {
+		   Resource resource = null;
+		   try {
+		      URI uriUri = URI.createURI(uri);
+		      Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		      resource = (new ResourceSetImpl()).createResource(uriUri);
+		      resource.getContents().add(root);
+		      resource.save(null);
+		   } catch (Exception e) {
+		      System.err.println("ERREUR sauvegarde du modèle : "+e);
+		      e.printStackTrace();
+		   }
+		}
+
+		public static Model chargerModele(String uri) {
+		   Resource resource = null;
+		   EPackage pack=UMLPackage.eINSTANCE;
+		   try {
+		      URI uriUri = URI.createURI(uri);
+		      Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("uml", new XMIResourceFactoryImpl());
+		      resource = (new ResourceSetImpl()).createResource(uriUri);
+		      XMLResource.XMLMap xmlMap = new XMLMapImpl();
+		      xmlMap.setNoNamespacePackage(pack);
+		      java.util.Map options = new java.util.HashMap();
+		      options.put(XMLResource.OPTION_XML_MAP, xmlMap);
+		      resource.load(options);
+		   }
+		   catch(Exception e) {
+		      System.err.println("ERREUR chargement du modèle : "+e);
+		      e.printStackTrace();
+		   }
+		   return (Model) resource.getContents().get(0);
+		}
+
+}
