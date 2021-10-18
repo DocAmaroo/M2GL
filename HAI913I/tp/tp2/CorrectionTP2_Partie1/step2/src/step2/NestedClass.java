c13cdcb972671b10b77049a5f1b122a496237216
C.Thomas <c.tom34270@gmail.com>
2021-10-18 23:41:52 +0200

    :sparkles: Update

---
+++ HAI913I/tp/tp2/CorrectionTP2_Partie1/step2/src/step2/NestedClass.java
@@ -0,0 +1,61 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Test file
+public class NestedClass {
+
+    private class Classe {
+
+    }
+
+}
+
+class DoubleClasse {
+
+}
+
+class A {
+
+    int a;
+    String b;
+    double k;
+    ArrayList<Integer> c = new ArrayList<>();
+}
+
+class B {}
+
+class C {}
+
+class D {}
+
+class E {
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+
+    public int getA() { return a; }
+    public int getB() { return b; }
+    public int getC() { return c; }
+    public int getD() { return d; }
+}
+
+class F {}
+
+class G {}
+
+class H {
+    double a;
+    String b;
+    int[] c;
+    List<String> d = new ArrayList<>();
+}
+
+class I {}
+
+class J {}
--- HAI913I/tp/tp2/CorrectionTP2_Partie1/step2/src/step2/Parser.java
+++ HAI913I/tp/tp2/CorrectionTP2_Partie1/step2/src/step2/Parser.java
@@ -2,11 +2,7 @@ package step2;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
@@ -44,7 +40,11 @@ public class Parser {
 		Set<String> packages = new HashSet<String>(); // use Set to force unique element
 		double totalLinesByMethods = 0;
 		double totalAttributs = 0;
-		
+
+		// List of classes and their number of methods
+		Map<String, Integer> classesMethods = new LinkedHashMap<>();
+		Map<String, Integer> classesAttributes = new LinkedHashMap<>();
+
 		//
 		for (File fileEntry : javaFiles) {
 			String content = FileUtils.readFileToString(fileEntry);
@@ -78,17 +78,45 @@ public class Parser {
 			
 			// Get the number of attributs
 			totalAttributs += getNumberOfAttributs(parse);
-			
+
+			// hashMap update
+			addClassMethods(parse, classesMethods);
+
+			// hashMap KEY: class - VALUE : nbAttributes update
+			addClassAttributes(parse, classesAttributes);
+
 		}
 		
-		System.out.println("[+] Number of classes: " + Math.ceil(totalAttributs));
+		System.out.println("[+] Number of classes: " + Math.ceil(totalClass));
 		System.out.println("[+] Number of methods: " + Math.ceil(totalMethods));
 		System.out.println("[+] Number of lines: " + Math.ceil(totalLines));
 		System.out.println("[+] Number of packages: " + packages.size());
-		System.out.println("[+] Average number of methods by class: " + Math.ceil(totalAttributs)/totalClass);
-		System.out.println("[+] Average number of lines by methods: " + Math.ceil(totalAttributs)/totalMethods);
+		System.out.println("[+] Average number of methods by class: " + Math.ceil(totalMethods/totalClass));
+		System.out.println("[+] Average number of lines by methods: " + Math.ceil(totalLinesByMethods/totalMethods));
 		System.out.println("[+] Average number of attributs by class: " + Math.ceil(totalAttributs/totalClass));
-		
+
+		// print all classes with more than X methods
+		// X is the second parameter of the function
+		// Always call the method classesWithMoreThanXMethods before classesWithMoreMethods
+		// because the last remove some key:values entries in the HashMap classesMethod
+		List<String> listClassesWithMoreThanXMethods = new ArrayList<>();
+		classesWithMoreThanXMethods(listClassesWithMoreThanXMethods, 4, classesMethods);
+
+		// print 10% classes with the more methods
+		int cmSize = classesMethods.size();
+		int nbToKeepM = (int) Math.ceil(cmSize*0.1);
+		List<String> listClassesWithMoreMethods = new ArrayList<>();
+		classesWithMoreMethods(listClassesWithMoreMethods, nbToKeepM, classesMethods);
+
+		// print 10% classes with the more attributs
+		int caSize = classesAttributes.size();
+		int nbToKeepA = (int) Math.ceil(cmSize*0.1);
+		List<String> listClassesWithMoreAttributes = new ArrayList<>();
+		classesWithMoreAttributes(listClassesWithMoreAttributes, nbToKeepA, classesAttributes);
+
+		// print classes belonging the two last ones
+		inBoth(listClassesWithMoreMethods, listClassesWithMoreAttributes);
+
 	}
 
 	// read all java files from specific folder
@@ -234,4 +262,104 @@ public class Parser {
 
 		return visitor.getAttribus().size();
 	}
+
+	// methods to add
+	// add KEY: ClassName - VALUE: nbMethods in a Map
+	public static void addClassMethods(CompilationUnit parse, Map<String, Integer> classesMethods) {
+		TypeDeclarationVisitor visitor = new TypeDeclarationVisitor();
+		parse.accept(visitor);
+		for (TypeDeclaration type : visitor.getTypes()) {
+			classesMethods.put(String.valueOf(type.getName()), type.getMethods().length);
+		}
+	}
+
+	public static void addClassAttributes(CompilationUnit parse, Map<String, Integer> classesAttributes) {
+		TypeDeclarationVisitor visitor = new TypeDeclarationVisitor();
+		parse.accept(visitor);
+		for (TypeDeclaration type : visitor.getTypes()) {
+			// System.out.println("########## type name : " + type.getName() + " " + type.getFields().length);
+			classesAttributes.put(String.valueOf(type.getName()), type.getFields().length);
+		}
+	}
+
+	// print 10% classes with more methods
+	public static void classesWithMoreMethods(List<String> listClassesWithMoreMethods, int nbToKeep, Map<String, Integer> classesMethods) {
+		while (listClassesWithMoreMethods.size() < nbToKeep) {
+			int max = Collections.max(classesMethods.values());
+			listClassesWithMoreMethods.addAll(
+					classesMethods.entrySet().stream()
+							.filter(entry -> entry.getValue() == max)
+							.map(entry -> entry.getKey())
+							.collect(Collectors.toList())
+			);
+			for (String type : listClassesWithMoreMethods) {
+				classesMethods.remove(type);
+			}
+		}
+		System.out.print("[+] Classes with more methods: ");
+		for (int i = 0; i < listClassesWithMoreMethods.size(); i++) {
+			System.out.print(listClassesWithMoreMethods.get(i) + " ");
+		}
+		System.out.println();
+	}
+
+	// print classes with more than X methods
+	public static void classesWithMoreThanXMethods(List<String> listClassesWithMoreThanXMethods, int x, Map<String, Integer> classesMethods) {
+		listClassesWithMoreThanXMethods.addAll(
+				classesMethods.entrySet().stream()
+						.filter(entry -> entry.getValue() > x)
+						.map(entry -> entry.getKey())
+						.collect(Collectors.toList())
+		);
+		System.out.print("[+] Classes with more than X methods: ");
+		for (int i = 0; i < listClassesWithMoreThanXMethods.size(); i++) {
+			System.out.print(listClassesWithMoreThanXMethods.get(i) + " ");
+		}
+		System.out.println();
+	}
+
+	// print 10% classes with more attributes
+	public static void classesWithMoreAttributes(List<String> listClassesWithMoreAttributes, int nbToKeepA, Map<String, Integer> classesAttributes) {
+		while (listClassesWithMoreAttributes.size() < nbToKeepA) {
+			int max = Collections.max(classesAttributes.values());
+			listClassesWithMoreAttributes.addAll(
+					classesAttributes.entrySet().stream()
+							.filter(entry -> entry.getValue() == max)
+							.map(entry -> entry.getKey())
+							.collect(Collectors.toList())
+			);
+			for (String type : listClassesWithMoreAttributes) {
+				classesAttributes.remove(type);
+			}
+		}
+		System.out.print("[+] Classes with more attibutes: ");
+		for (int i = 0; i < listClassesWithMoreAttributes.size(); i++) {
+			System.out.print(listClassesWithMoreAttributes.get(i) + " ");
+		}
+		System.out.println();
+	}
+
+	public static void inBoth(List<String> listClassesWithMoreMethods, List<String> listClassesWithMoreAttributes) {
+		List<String> listClassesInBoth = new ArrayList<>();
+		// we go through the shortest list
+		if (listClassesWithMoreMethods.size() <= listClassesWithMoreAttributes.size()) {
+			for (int i = 0; i < listClassesWithMoreMethods.size(); i++) {
+				if (listClassesWithMoreAttributes.contains(listClassesWithMoreMethods.get(i))) {
+					listClassesInBoth.add(listClassesWithMoreMethods.get(i));
+				}
+			}
+		} else {
+			for (int i = 0; i < listClassesWithMoreAttributes.size(); i++) {
+				if (listClassesWithMoreMethods.contains(listClassesWithMoreAttributes.get(i))) {
+					listClassesInBoth.add(listClassesWithMoreAttributes.get(i));
+				}
+			}
+		}
+		System.out.print("[+] Classes in both categories: ");
+		for (int i = 0; i < listClassesInBoth.size(); i++) {
+			System.out.print(listClassesInBoth.get(i) + " ");
+		}
+		System.out.println();
+	}
+
 }
